966f6c651dcefff3cfd64a6daf1cfb199b47e39c
TheNekOz <25076443+TheNekOz@users.noreply.github.com>
2026-01-12 14:52:00 +0100

    Completed Assignment

--- src/main/java/assignment/Search.java
+++ src/main/java/assignment/Search.java
@@ -1,11 +1,32 @@
 package assignment;
 
+import java.util.Arrays;
+
 public class Search {
     int[] arr;
     public Search(int[] x) {
         arr = x;
     }
-    public linearSearch(int x) {
-        arr = new int[x];
+    public int linearSearch(int x) {
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int stepsLinearSearch(int x) {
+        int steps = 0;
+        int length = arr.length;
+        System.out.println("Length: " + length);
+        for (int i = 0; i < length; i++) {
+            steps++;
+            if (arr[i] == x) {
+                return steps;
+            }
+        }
+        return steps;
     }
 }
+
