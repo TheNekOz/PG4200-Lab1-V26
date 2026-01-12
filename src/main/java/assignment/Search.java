78c7b7253ffbc5f88ca70e05d3269ddefc54b69c
NekOz <25076443+TheNekOz@users.noreply.github.com>
2026-01-12 13:47:10 +0100

    Start of constructor

--- src/main/java/assignment/Search.java
+++ src/main/java/assignment/Search.java
@@ -1,4 +1,11 @@
 package assignment;
 
 public class Search {
+    int[] arr;
+    public Search(int[] x) {
+        arr = x;
+    }
+    public linearSearch(int x) {
+        arr = new int[x];
+    }
 }
